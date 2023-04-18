78106e671fd4caaadff280a94c47c2768866174a
Chloe Wahl-Dassule <cwahldassule c:; cd 'c:\Users\Chloe\OneDrive\Documents\CSC 120 files\CSC120-git-demo'; & 'C:\Users\Chloe\AppData\Local\Programs\Python\Python310\python.exe' 'c:\Users\Chloe\.vscode\extensions\ms-python.python-2022.20.2\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '61049' '--' 'extension-output-vscode.git-#1-Git' @smith.edu>
2023-04-18 12:44:38 -0400

    Compiles

--- Garden.java
+++ Garden.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class Garden{
   FairyVillage fairyVillage;
   Bumblebee user = null;
+  int upper_bound;
+  int right_bound;
 
   public Garden(String name, String type){
+    this.upper_bound = 3;
+    this.right_bound = 3;
     this.fairyVillage = new FairyVillage("Fairy Village");
     if(type.equals("BUMBLEBEE")){
       this.user = new Bumblebee(name);
@@ -26,8 +30,14 @@ public class Garden{
     Scanner in = new Scanner(System.in);
     String input = in.nextLine();
    
-    if(user.user_pos_x == fairyVillage.loc_x){
-
+    if(user.user_pos_x == fairyVillage.loc_x & user.user_pos_y == fairyVillage.loc_y){
+      while(true){
+        boolean temp = false;
+        temp = fairyVillage.play();
+        if(!temp){
+          break;
+        }
+      }
     }
     //create if/if else statement for each user option
     
@@ -36,7 +46,7 @@ public class Garden{
 
   public static void main(String[] args) {
     System.out.println("intro");
-    Garden myGarden = new Garden("Chloe", );
+    Garden myGarden = new Garden("Chloe", "BUMBLEBEE" );
     //user input for what kind of character
     
     while(true){
